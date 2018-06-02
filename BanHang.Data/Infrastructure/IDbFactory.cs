0ad5787a57a2538b4bc782e51346ecc11c32c12a
ADMIN\Admin <hoangsan0303@gmail.com>
2018-06-03 08:19:48 +0900

    add entity framework code first for data

--- BanHang.Data/Infrastructure/IDbFactory.cs
+++ BanHang.Data/Infrastructure/IDbFactory.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BanHang.Data.Infrastructure
 {
-    interface IDbFactory
+    public interface IDbFactory : IDisposable
     {
+        BanHangDbContext Init();
     }
 }
---
+++ BanHang.Data/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BanHang.Data.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IDbFactory dbFactory;
+
+        private BanHangDbContext dbContext;
+
+        public UnitOfWork(IDbFactory dbFactory)
+        {
+            this.dbFactory = dbFactory;
+        }
+
+        public BanHangDbContext DbContext
+        {
+            get { return dbContext ?? (dbContext = dbFactory.Init()); }
+        }
+
+        public void commit()
+        {
+            DbContext.SaveChanges();
+        }
+    }
+}
