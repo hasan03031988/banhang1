b50960dce37809950a64ba8e6d7e512809ba03bb
ADMIN\Admin <hoangsan0303@gmail.com>
2018-06-03 22:41:05 +0900

    change data project

---
+++ BanHang.Service/PostService.cs
@@ -0,0 +1,77 @@
+﻿using BanHang.Data.Infrastructure;
+using BanHang.Data.Repositories;
+using BanHang.Model.Models;
+using System;
+using System.Collections.Generic;
+
+namespace BanHang.Service
+{
+    public interface IPostService
+    {
+        void Add(Post post);
+
+        void Update(Post post);
+
+        void Delete(Post post);
+
+        IEnumerable<Post> GetAll();
+
+        IEnumerable<Post> GetAllPaging(int page, int pageSize, out int totalRow);
+
+        Post GetById(int id);
+
+        IEnumerable<Post> GetAllByTagPaging(int page, int pageSize, out int totalRow);
+
+        void SaveChanges();
+    }
+
+    public class PostService : IPostService
+    {
+        IPostRepository _postRepository;
+        IUnitOfWork _unitOfWork;
+        public PostService(IPostRepository postRepository,IUnitOfWork unitOfWork)
+        {
+            this._postRepository = postRepository;
+            this._unitOfWork = unitOfWork;
+        }
+        public void Add(Post post)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Post post)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Post> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Post> GetAllByTagPaging(int page, int pageSize, out int totalRow)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Post> GetAllPaging(int page, int pageSize, out int totalRow)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Post GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Post post)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
